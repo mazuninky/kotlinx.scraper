5e8e78600842b844c287a9f92a573f6a5ce0383a
Mazunin Konstantin <mazuninky@gmail.com>
2019-11-24 01:48:55 +0300

    Add cache for Gradle CI workflow close #1

--- scraper/src/commonMain/kotlin/kotlinx/scraper/provider/Provider.kt
+++ scraper/src/commonMain/kotlin/kotlinx/scraper/provider/Provider.kt
@@ -1,7 +1,6 @@
 package kotlinx.scraper.provider
 
 import kotlinx.scraper.Source
-import kotlinx.scraper.WebPage
 
 abstract class Provider<S : Source> {
     abstract suspend fun fetch(source: S): String
